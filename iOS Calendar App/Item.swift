92c3fbf46a0b2d8da992feeed3a4eca38b559172
Fadi Al Eliwi <fadieliwi@gmail.com>
2024-12-09 15:54:21 +0100

    setup

---
+++ iOS Calendar App/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  iOS Calendar App
+//
+//  Created by Fadi Al Eliwi on 09.12.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
